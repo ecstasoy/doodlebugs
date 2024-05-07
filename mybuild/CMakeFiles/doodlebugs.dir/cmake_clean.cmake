d1c673a51f0382f3e9b56ca92534ce7926fe6398
Kunhua Huang <huang.kunh@northeastern.edu>
2024-05-07 07:23:28 -0400

    CMake added

---
+++ mybuild/CMakeFiles/doodlebugs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doodlebugs.dir/doodlebugs.cpp.o"
+  "CMakeFiles/doodlebugs.dir/doodlebugs.cpp.o.d"
+  "CMakeFiles/doodlebugs.dir/game.cpp.o"
+  "CMakeFiles/doodlebugs.dir/game.cpp.o.d"
+  "CMakeFiles/doodlebugs.dir/organism.cpp.o"
+  "CMakeFiles/doodlebugs.dir/organism.cpp.o.d"
+  "doodlebugs"
+  "doodlebugs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doodlebugs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
